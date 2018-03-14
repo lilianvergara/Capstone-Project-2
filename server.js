2b53224b7b03a0f5e450e4241f5a3a75ca11659f
Lily <vergara.lilian0@gmail.com>
2018-03-13 20:39:34 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+//Need to run all "npm install " for modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+
+const apiRouter = require('./api/api');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api', apiRouter);
+
+app.use(errorhandler());
+
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
+
+module.exports = app;
